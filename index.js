3395fe99e252870c94b00f67229eee35c67d7a57
Zsannen Mariano <zsannen.mariano@freedom.tm>
2019-12-19 10:11:47 +0800

    Initial tiktok downloader

---
+++ index.js
@@ -0,0 +1,129 @@
+const { default: TikTokAPI, getRequestParams } = require('tiktok-api');
+const crypto = require('crypto');
+const _ = require('lodash');
+const wget = require('wget-improved');
+const urlRegex = require("url-regex");
+const fetch = require("node-fetch");
+const https = require('https');
+const fs = require('fs');
+
+// https://github.com/sudoguy/tiktok_bot/blob/113607be2108fbc56c39ac174557374897e470f8/tiktok_bot/client/utils.py
+
+const signURL = async (url, ts, deviceId) => {
+    console.log(url, ts, deviceId);
+
+    let as_str = String(ts);
+    let cp_str = as_str + String(Date.now());
+
+    let as = crypto.createHash('md5').update(as_str).digest('hex');
+    let cp = crypto.createHash('md5').update(cp_str).digest('hex');
+    let mas_sha = crypto.createHash('sha1').update(as_str).digest('hex');
+    let mas = crypto.createHash('md5').update(mas_sha).digest('hex');
+
+    as = 'a1qwert123';
+    cp = 'cbfhckdckkde1';
+    let final_url = `${url}&as=${as}&cp=${cp}`;
+    console.log(final_url);
+    return final_url;
+};
+
+// Required - device parameters
+// You need to source these using a man-in-the-middle proxy such as mitmproxy,
+// CharlesProxy or PacketCapture (Android)
+const params = getRequestParams({
+    // https://github.com/sudoguy/tiktok_bot/blob/bc486ac3c0d12b8c1773d05329fca9374fab0d51/tiktok_bot/api/config.py
+    // https://github.com/tolgatasci/musically-tiktok-api-python/wiki/Device-Info-Change
+    // suspended https://coding.vivoliker.com/embed/OLP95IXSOo
+    device_id:
+    // '666238484913368736', // repeated request gets the device id blocked
+    // '666238489691542860', // was valid, blocked after 3 requests
+        '66623848' + (_.random(1000000000, 10000000000)), // randomizing the device_id seems to allow request not requiring auth to go through '6662384847253865990'
+    // even randomized device id sometimes doesn't work
+    fp: '',
+    iid: '6749111388298184454',
+    // openudid: 'a0b7148b6e5edcb1',
+    // device_platform: 'android' + _.random(1, 10000),
+
+});
+
+const api = new TikTokAPI(params, { signURL });
+// not working without auth
+// api.listPosts({
+//         user_id: '5024672',
+//         count: 1,
+//         cursor: 0,
+//     })
+//     .then(res => {
+//         if (res.data) {
+//             console.log(res.data)
+//         }
+//     })
+//     .catch(console.log);
+
+// works without proper signing
+// https://gist.github.com/pandafox/c19ad740d53d1da9b2c73ebf3d05f0e3#file-sample_post_detail-json
+api.getPost('6769511215174601990')
+    .then(res => {
+        //console.log('RES = ', res);
+        let watermark_url = _.find(_.get(res, 'data.aweme_detail.video.download_addr.url_list'),
+            url => _.includes(url, 'watermark'));
+        if (!watermark_url) {
+            console.log('device id blocked');
+        } else {
+            // duration of the video in ms
+            // _.find(_.get(res, 'data.aweme_detail.duration') / 1000
+
+            // music only URL
+            // _.first(_.get(res, 'data.aweme_detail.music.play_url.url_list'))
+
+            // remove watermark, and use highest quality
+            let video_url = watermark_url
+                .replace('watermark=1', 'watermark=0')
+                .replace('bitrate=0', 'bitrate=1');
+
+            uploadLocally(video_url, `${res.data.aweme_detail.aweme_id}.mp4`);
+        }
+    })
+    .catch(err => {
+        console.log("ERROR", error);
+    });
+
+const uploadLocally = function (url, id) {
+    var outputFolder = './downloads/';
+    if (!outputFolder.endsWith('/')) outputFolder += '/';
+
+    console.log(`Output folder > ${outputFolder}`);
+    console.log(`URL > ${url}`);
+
+    fetch(url)
+        .then(res => {
+            return res.text();
+        })
+        .then(body => {
+            const urls = body.match(urlRegex());
+            const mediaUrl = urls.find(url => url.includes("muscdn.com") && url.includes("https://v"));
+            console.log(`Found Media URL: ${mediaUrl}`);
+
+            const file = fs.createWriteStream(`${outputFolder}${id}`);
+            const request = https.get(url, function(response) {
+                response.pipe(file);
+            });
+        });
+
+}
+
+// You are now able to make successful requests
+// api.listForYouFeed()
+//     .then(res => console.log(res.data))
+//     .catch(console.log);
+
+// api.searchUsers({
+//         keyword: 'nianaguerrero',
+//         count: 1,
+//         cursor: 0,
+//     })
+//     .then(res => console.log(res.data))
+//     .catch(console.log);
+// api.getUser('rebeccacornford')
+//     .then(res => console.log(res.data))
+//     .catch(console.log);og("listPosts = ", res.data.aweme_list))

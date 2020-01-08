e9ef9b942496b7653f8e42253ce967796350f3c2
Zsannen Mariano <zsannen.mariano@freedom.tm>
2020-01-08 13:24:47 +0800

    Testing device id generator

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const fs = require('fs');
 // https://github.com/sudoguy/tiktok_bot/blob/113607be2108fbc56c39ac174557374897e470f8/tiktok_bot/client/utils.py
 
 var retryCount = 0;
-var maxRetries = 5;
+var maxRetries = 1000;
+
+var deviceIdPrefix = 0;
+var iId = 0;
+var maxRetryDeviceId = 10;
+var retryDeviceIdCount = 0;
 
 const uploadLocally = function (url, id) {
     var outputFolder = './downloads/';
@@ -44,11 +49,24 @@ const signURL = async (url, ts, deviceId) => {
     let mas_sha = crypto.createHash('sha1').update(as_str).digest('hex');
     let mas = crypto.createHash('md5').update(mas_sha).digest('hex');
 
-    as = 'a1qwert123';
-    cp = 'cbfhckdckkde1';
-    let final_url = `${url}&as=${as}&cp=${cp}`;
+    let final_url = `${url}&as=${as}&cp=${cp}&mas=${mas}`;
     console.log(final_url);
     return final_url;
+};
+
+const generateDeviceId = function () {
+    retryDeviceIdCount = 0;
+
+    const _deviceId = _.random(60000000, 99999999);
+    const suffix = _.random(1000000000, 10000000000);
+
+    deviceIdPrefix = _deviceId;
+    iId = `${_deviceId}${suffix}`;
+
+    console.log("");
+    console.log('DEVICE ID PREFIX = ', deviceIdPrefix);
+    console.log('IID = ', iId);
+    console.log("");
 };
 
 // Required - device parameters
@@ -61,35 +79,76 @@ const params = getRequestParams({
     device_id:
     // '666238484913368736', // repeated request gets the device id blocked
     // '666238489691542860', // was valid, blocked after 3 requests
-        '66623848' + (_.random(1000000000, 10000000000)), // randomizing the device_id seems to allow request not requiring auth to go through '6662384847253865990'
+    // `73324957${(_.random(1000000000, 10000000000))}`,
+    deviceIdPrefix,
+    //     '67491113' + (_.random(1000000000, 10000000000)), // randomizing the device_id seems to allow request not requiring auth to go through '6662384847253865990'
     // even randomized device id sometimes doesn't work
-    fp: '',
-    iid: '6749111388298184454',
+    // iid: `733249576192440920`,
+    iid: iId,
     // openudid: 'a0b7148b6e5edcb1',
     // device_platform: 'android' + _.random(1, 10000),
-
+    ssmix: "a",
+    manifest_version_code: "2018111632",
+    dpi: 420,
+    app_name: "musical_ly",
+    version_name: "9.1.0",
+    is_my_cn: 0,
+    ac: "wifi",
+    update_version_code: "2018111632",
+    channel: "googleplay",
+    build_number: "9.9.0",
+    version_code: 910,
+    resolution: "1080*1920",
+    mcc_mnc: "23001",
+    is_my_cn: 0,
+    fp: "",
+    app_type: "normal",
 });
 
-const api = new TikTokAPI(params, { signURL });
+const api = function (changeDeviceId = false) {
+    retryDeviceIdCount++;
 
-const uploadTiktokVideo = function (postId) {
+    if (deviceIdPrefix == 0) {
+        generateDeviceId();
+    }
+
+    if (retryDeviceIdCount >= maxRetryDeviceId) {
+        generateDeviceId();
+    }
+
+    return new TikTokAPI(params, { signURL })
+};
+
+const uploadTiktokVideo = function (postId, changeDeviceId = false) {
     retryCount++;
+
+    console.log("");
+    console.log("RETRY COUNT = ", retryCount);
+    console.log("");
 
     if (retryCount > maxRetries) {
         console.log("");
         console.log("Message: Max retries have reached. Please try to run it again.");
         console.log("");
     } else {
-        api.getPost(postId)
+        const _api = api(changeDeviceId);
+        _api.getPost(postId)
             .then(res => {
-                //console.log('RES = ', res);
+                console.log("");
+                console.log('RES = ', _.get(res, 'data'));
+                console.log("");
                 let watermark_url = _.find(_.get(res, 'data.aweme_detail.video.download_addr.url_list'),
                     url => _.includes(url, 'watermark'));
 
                 if (!watermark_url) {
+                    console.log("");
+                    console.log("");
                     console.log('Device ID blocked');
                     console.log('Retrying...');
-                    uploadTiktokVideo(postId);
+                    console.log("");
+                    console.log("");
+
+                    uploadTiktokVideo(postId, true);
                 } else {
                     // duration of the video in ms
                     // _.find(_.get(res, 'data.aweme_detail.duration') / 1000
@@ -98,6 +157,12 @@ const uploadTiktokVideo = function (postId) {
                     // _.first(_.get(res, 'data.aweme_detail.music.play_url.url_list'))
 
                     // remove watermark, and use highest quality
+
+                    console.log("");
+                    console.log('GLOBAL DEVICE ID PREFIX = ', deviceIdPrefix);
+                    console.log('GLOBAL IID = ', iId);
+                    console.log("");
+
                     let video_url = watermark_url
                         .replace('watermark=1', 'watermark=0')
                         .replace('bitrate=0', 'bitrate=1');
